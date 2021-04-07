3ba873811eb4152bd2d36f2a14b91010384c278e
mjmsiv <v_wangmaofeng@saicmotor.com>
2021-04-07 14:47:49 +0800

    哈哈哈
    图片测试

---
+++ PictureTest/PictureTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PictureTest
+//
+//  Created by maofeng.wang on 2021/4/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PictureTest/PictureTest/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PictureTest
+//
+//  Created by maofeng.wang on 2021/4/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
